54481806ba0d17411094f12e1cd9953de0c7ec59
Faizzroi <roihanfaizz@gmail.com>
2023-02-07 17:19:10 +0700

    Js

--- Js/function.js
+++ Js/function.js
@@ -6,10 +6,10 @@ console.log(kotak);
 
 const luasPersegi = (panjang=0 , lebar=0) => panjang*lebar
 
-const luasPersegi2 = (panjang, lebar) => {
-    hasil = this[0] * this[1]
-    return hasil
-}
+// const luasPersegi2 = (panjang, lebar) => {
+//     hasil = this[0] * this[1]
+//     return hasil
+// }
 
 const volumeBalok = (panjang=0 , lebar=0, tinggi=0) => {
     const hasil=panjang*lebar*tinggi
